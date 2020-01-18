c43a363c571b9734fe15c49c170c7821ad5960e2
Neeraj Kumar <nraj_adi@yahoo.co.in>
2020-01-18 15:37:09 +0530

    advices are added to the aspect.

--- aopApp/src/main/java/com/ducat/aspects/ABCAspect.java
+++ aopApp/src/main/java/com/ducat/aspects/ABCAspect.java
@@ -1,5 +1,11 @@
 package com.ducat.aspects;
 
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -22,9 +28,48 @@ public class ABCAspect {
 	public void pc3() {}
 	
 	// Before advice
-	@Before("pc1() || pc2() || pc3()")
-	public void preProcessing()
+	@Before("pc1() || pc3()")
+	public void preProcessing(JoinPoint jp)
 	{
-		System.out.println("Before advice is applied.");
+		String m = jp.getSignature().getName()+"()";
+		System.out.println("Before advice is applied on "+m);
 	}
+	// Throws advice
+	@AfterThrowing(value="pc3()",throwing="ex")
+	public void errorProcessing(JoinPoint jp, Exception ex)
+	{
+		String m = jp.getSignature().getName()+"()";
+		System.out.println("Throws advice is applied on "
+		+m+" because of "+ex);
+	}
+	
+	// After advice
+	/*@AfterReturning(value="pc2()",returning="rvalue")
+	public void postProcessing(JoinPoint jp, Object rvalue)
+	{
+		String m = jp.getSignature().getName()+"()";
+		System.out.println("After advice is applied on "
+		+m+" when "+rvalue+" is returned by it.");
+		rvalue="failure";
+		System.out.println("Return value is changed to failure by the advice.");
+		
+	}*/
+// Around advice
+	@Around(value="pc2()")
+	public Object aroundProcessing(ProceedingJoinPoint jp) throws Throwable
+	{
+		// pre processing
+		String m = jp.getSignature().getName()+"()";
+		System.out.println("Around advice is applied on "
+		+m);
+		//return "short circuited";
+		//Getting the method invoked on target
+		Object rvalue= jp.proceed();
+		// post processing
+		System.out.println(rvalue+" is returned by "+m);
+		rvalue="failure";
+		System.out.println("Return value is changed to failure by the advice.");
+		return rvalue;
+	}	
+	
 }
